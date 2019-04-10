71d2f9828c01847cc8b027e900add09a3e1e5cda
VarunPai7 <48701555+VarunPai7@users.noreply.github.com>
2019-04-09 20:18:13 -0400

    Initilization Classes

---
+++ ImageFilter.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+import java.util.*;
+public class ImageFilter extends FileFilter {
+    public boolean accept(File f) {
+        if (f.isDirectory()) {
+            return true;
+        }
+        String extension = getExtension(f);
+        if (extension != null) {
+            if (extension.equals("tif") ||
+                extension.equals("nd2"))  {
+                    return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+    public String getDescription() {
+        return ".ND2 or .tif";
+    }	
+    public static String getExtension(File f) {
+        String ext = null;
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 &&  i < s.length() - 1) {
+            ext = s.substring(i+1).toLowerCase();
+        }
+        return ext;
+    }
+}
---
+++ Initialization.java
@@ -0,0 +1,89 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import java.util.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class Initialization {
+
+	private JFrame frmInitialization;
+	public boolean netTrain;
+	public boolean cellIdent;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Initialization window = new Initialization();
+					window.frmInitialization.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Initialization() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmInitialization = new JFrame();
+		frmInitialization.setTitle("Initialization");
+		frmInitialization.setBounds(100, 100, 592, 435);
+		frmInitialization.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmInitialization.getContentPane().setLayout(null);
+		
+		JLabel lblWelcomeToThe = new JLabel("Welcome to the Convolutional Neural Network!");
+		lblWelcomeToThe.setBounds(15, 16, 398, 20);
+		frmInitialization.getContentPane().add(lblWelcomeToThe);
+		
+		JLabel lblPleaseSelectWhich = new JLabel("Please select which Network function you would like to use today.");
+		lblPleaseSelectWhich.setBounds(15, 52, 537, 20);
+		frmInitialization.getContentPane().add(lblPleaseSelectWhich);
+
+		JRadioButton rdbtnNetworkTraining = new JRadioButton("NETWORK TRAINING");
+		rdbtnNetworkTraining.setBounds(15, 84, 222, 29);
+		frmInitialization.getContentPane().add(rdbtnNetworkTraining);
+		rdbtnNetworkTraining.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				netTrain = true;
+			}
+		});
+		JRadioButton rdbtnNetworkCellIdentification = new JRadioButton("NETWORK CELL IDENTIFICATION");
+		rdbtnNetworkCellIdentification.setBounds(15, 121, 301, 29);
+		frmInitialization.getContentPane().add(rdbtnNetworkCellIdentification);
+		rdbtnNetworkCellIdentification.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cellIdent = true;
+			}
+		});
+		JButton btnConfirm = new JButton("CONFIRM");
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frmInitialization.setVisible(false);
+				if(cellIdent) {
+					ChannelSelection channel = new ChannelSelection('I');
+				} else {
+					if(netTrain) {
+						ChannelSelection channel = new ChannelSelection('T');
+					}
+				}
+			}
+		});
+		btnConfirm.setBounds(201, 162, 115, 29);
+		frmInitialization.getContentPane().add(btnConfirm);	
+		
+	}
+}
